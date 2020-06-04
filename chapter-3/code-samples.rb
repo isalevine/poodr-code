df3ab1cff43a0d1328f5f6a0d5afaf02aa12a807
isalevine <isa.loves.coding@gmail.com>
2020-06-03 23:48:54 -0700

    listing 3.3

--- chapter-3/code-samples.rb
+++ chapter-3/code-samples.rb
@@ -58,3 +58,25 @@ end
 
 puts Gear.new(52, 11, 26, 1.5).gear_inches
 #=> 137.0909090909091
+
+
+# Listing 3.3, pg. 43
+
+class Gear
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring, cog, wheel)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+  # ...
+end
+
+# Gear expects a 'Duck' that knows 'diameter'
+puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
+#=> 137.0909090909091
