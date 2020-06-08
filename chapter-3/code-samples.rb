9c58e89f55c35c5c971e10019dea9f28a2bb12db
isalevine <isa.loves.coding@gmail.com>
2020-06-07 23:46:44 -0700

    listing 3.13

--- chapter-3/code-samples.rb
+++ chapter-3/code-samples.rb
@@ -229,3 +229,20 @@ puts Gear.new(
   chainring: 52,
   cog: 11).gear_inches
 #=> 137.0909090909091
+
+
+# Listing 3.13, pg. 50-51
+
+class Gear
+  attr_reader :chainring, :cog, :wheel
+
+  def initialize(chainring: 40, cog: 18, wheel:)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+  end
+  # ...
+end
+
+puts Gear.new(wheel: Wheel.new(26, 1.5)).chainring
+#=> 40
