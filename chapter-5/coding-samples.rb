d4c738eee7e1c42d89f18278a67388e27f53d60f
isalevine <isa.loves.coding@gmail.com>
2020-06-15 23:12:18 -0700

    listing 5.4

--- chapter-5/coding-samples.rb
+++ chapter-5/coding-samples.rb
@@ -99,3 +99,23 @@ class Driver
   end
   # ...
 end
+
+
+# Listing 5.4, pg. 96
+
+class Trip
+  def prepare(preparers)
+    preparers.each { |preparer|
+      case preparer
+      when Mechanic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoordinator
+        preparer.buy_food(customers)
+      when Driver
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank(vehicle)
+      end
+    }
+  end
+  # ...
+end
