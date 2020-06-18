541db6ec013eadc0e4be841125d837c5b44309bd
isalevine <isa.loves.coding@gmail.com>
2020-06-18 10:47:40 -0700

    listing 5.7

--- chapter-5/coding-samples.rb
+++ chapter-5/coding-samples.rb
@@ -157,3 +157,27 @@ end
     preparer.gas_up(vehicle)
     preparer.fill_water_tank(vehicle)
   end
+
+
+# Listing 5.7, pg. 99
+
+def find_with_ids(*ids)
+  raise UnknownPrimaryKey.new(@klass) if primary_key.nil?
+
+  expects_array = ids.first.kind_of?(array)
+  return ids.first if expects_array && ids.first.empty?
+
+  ids = ids.flatten.compact.uniq
+
+  case ids.size
+  when 0
+    raise RecordNotFound, "Couldn't find #{@klass.name} without an ID"
+  when 1
+    result = find_one(ids.first)
+    expects_array ? [ result ] : result
+  else
+    find_some(ids)
+  end
+rescue ::RangeError
+  raise RecordNotFound, "Couldn't find #{@klass.name} with an out of range ID"
+end
