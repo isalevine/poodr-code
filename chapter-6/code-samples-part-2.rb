535029a185961ffc768cb1afb2e8431a029fc16c
isalevine <isa.loves.coding@gmail.com>
2020-06-30 23:08:26 -0700

    listing 6.26

--- chapter-6/code-samples-part-2.rb
+++ chapter-6/code-samples-part-2.rb
@@ -399,3 +399,26 @@ class RoadBike < Bicycle
     @tape_color = opts[:tape_color]
   end
 end
+
+
+# Listing 6.26, pg. 135-136
+
+class Bicycle
+  # ...
+  def spares
+    { tire_size: tire_size,
+      chain: chain }.merge(local_spares)
+  end
+
+  # hook for subclasses to override
+  def local_spares
+    {}
+  end
+end
+
+class RoadBike < Bicycle
+  # ...
+  def local_spares
+    { tape_color: tape_color }
+  end
+end
