b6abe625ca1f8ce60a4a55ae77108fbe14ef24cf
isalevine <isa.loves.coding@gmail.com>
2020-07-04 21:17:13 -0700

    listing 7.1

---
+++ chapter-7/code-samples.rb
@@ -0,0 +1,15 @@
+# Listing 7.1, pg. 144
+
+class Schedule
+  def scheduled?(schedulable, starting, ending)
+    # ...
+  end
+
+  def add(target, starting, ending)
+    # ...
+  end
+
+  def remove(target, starting, ending)
+    # ...
+  end
+end
