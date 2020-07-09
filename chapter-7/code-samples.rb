2e34bcba2392e79dee4a089426ed33e42f9e35d5
isalevine <isa.loves.coding@gmail.com>
2020-07-08 23:19:09 -0700

    listing 7.5

--- chapter-7/code-samples.rb
+++ chapter-7/code-samples.rb
@@ -87,3 +87,24 @@ module Schedulable
     0
   end
 end
+
+
+# Listing 7.5, pg. 151
+
+class Bicycle
+  include Schedulable
+  
+  def lead_days
+    1
+  end
+  # ...
+end
+
+require 'date'
+starting = Date.parse("2019/09/04")
+ending = Date.parse("2019/09/10")
+
+b = Bicycle.new
+puts b.schedulable?(starting, ending)
+#=> This Bicycle is available 2019-09-03 - 2019-09-10
+#=> true
