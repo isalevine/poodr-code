a3c1c681f259c3f6b62c07fd1bab774341fdb0c9
isalevine <isa.loves.coding@gmail.com>
2020-07-10 21:10:52 -0700

    listing 8.1

---
+++ chapter-8/code-samples.rb
@@ -0,0 +1,14 @@
+# Listing 8.1, pg. 165
+
+class Bicycle
+  attr_reader :size, :parts
+
+  def initialize(size:, parts:)
+    @size = size
+    @parts = parts
+  end
+
+  def spares
+    parts.spares
+  end
+end
