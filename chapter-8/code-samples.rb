31801dea5c2192630e993b30bbe33a14d4c58998
isalevine <isa.loves.coding@gmail.com>
2020-07-11 21:40:35 -0700

    listing 8.2

--- chapter-8/code-samples.rb
+++ chapter-8/code-samples.rb
@@ -12,3 +12,73 @@ class Bicycle
     parts.spares
   end
 end
+
+
+# Listing 8.2, pg. 165-166
+
+class Parts
+  attr_reader :chain, :tire_size
+
+  def initialize(**opts)
+    @chain = opts[:chain] || default_chain
+    @tire_size = opts[:tire_size] || default_tire_size
+    post_initialize(opts)
+  end
+
+  def spares
+    { chain: chain,
+      tire_size: sire_size }.merge(local_spares)
+  end
+
+  def default_tire_size
+    raise NotImplementedError
+  end
+
+  # subclasses may override
+  def post_initialize(opts)
+    nil
+  end
+
+  def local_spares
+    {}
+  end
+
+  def default_chain
+    "11-speed"
+  end
+end
+
+
+class RoadBikeParts < Parts
+  attr_reader :tape_color
+
+  def post_initialize(**opts)
+    @tape_color = opts[:tape_color]
+  end
+
+  def local_spares
+    { tape_color: tape_color }
+  end
+
+  def default_tire_size
+    "23"
+  end
+end
+
+
+class MountainBikeParts < Parts
+  attr_reader :front_shock, :rear_shock
+
+  def post_initialize(**opts)
+    @front_shock = opts[:front_shock]
+    @rear_shock = opts[:rear_shock]
+  end
+
+  def local_spares
+    { front_shock: front_shock }
+  end
+
+  def default_tire_size
+    "2.1"
+  end
+end
