49f56fd855139456434667aad6f3e7697f711224
isalevine <isa.loves.coding@gmail.com>
2020-07-22 22:51:19 -0700

    listing 8.11

--- chapter-8/code-samples-pt-2.rb
+++ chapter-8/code-samples-pt-2.rb
@@ -1,3 +1,11 @@
+# Listing 8.9, pg. 173
+
+# code within Parts
+  def size
+    parts.size
+  end
+
+
 # Listing 8.10, pg. 173
 
 class Parts < Array
@@ -7,19 +15,62 @@ class Parts < Array
 end
 
 
+# copied code from part 1 (moved to prevent conflicts with Parts inheriting from Array or not)
+
+class Bicycle
+  attr_reader :size, :parts
+
+  def initialize(size:, parts:)
+    @size = size
+    @parts = parts
+  end
+
+  def spares
+    parts.spares
+  end
+end
+
+class Part
+  attr_reader :name, :description, :needs_spares
+
+  def initialize(name:, description:, needs_spares: true)
+    @name = name
+    @description = description
+    @needs_spares = needs_spares
+  end
+end
+
+chain = Part.new(name: "chain", description: "11-speed")
+road_tire = Part.new(name: "tire_size", description: "23")
+tape = Part.new(name: "tape_color", description: "red")
+mountain_tire = Part.new(name: "tire_size", description: "2.1")
+rear_shock = Part.new(name: "rear_shock", description: "Fox", needs_spares: false)
+front_shock = Part.new(name: "front_shock", description: "Manitou")
+
+road_bike_parts = Parts.new([chain, road_tire, tape])
+mountain_bike_parts = Parts.new([chain, mountain_tire, front_shock, rear_shock])
+
+road_bike = Bicycle.new(size: "L", parts: Parts.new([chain, road_tire, tape]))
+mountain_bike = Bicycle.new(size: "L", parts: Parts.new([chain, mountain_tire, front_shock, rear_shock]))
+
+
 # Listing 8.11, pg. 174
 
 # Parts inherits `+` from Array, so you can
 # add two Parts together
 combo_parts = (mountain_bike.parts + road_bike.parts)
 
-puts mountain_bike.parts.class  #=>
-puts road_bike.parts.class      #=>
-puts combo_parts.class          #=>
+puts mountain_bike.parts.class  #=> Parts
+puts road_bike.parts.class      #=> Parts
+puts combo_parts.class          #=> Array
 
 # `+` definitely combines the Parts...
-puts combo_parts.size           #=>
+puts combo_parts.size           #=> 7
 
 # ...but the object that `+` returns
 # does not understand `spares`
-puts combo_parts.spares         #=>
+begin
+  puts combo_parts.spares         #=> code-samples-pt-2.rb:73:in `<main>': undefined method `spares' for #<Array:0x00007fa4cb098540> (NoMethodError)
+rescue NoMethodError
+  puts "code-samples-pt-2.rb:73:in `<main>': undefined method `spares' for #<Array:0x00007fa4cb098540> (NoMethodError)"
+end
