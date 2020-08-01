2548fd0b1e8b06d63b05afb364a1d7140032bf80
isalevine <isa.loves.coding@gmail.com>
2020-07-31 22:31:48 -0700

    listing 8.15

--- chapter-8/code-samples-pt-2.rb
+++ chapter-8/code-samples-pt-2.rb
@@ -115,3 +115,19 @@ mountain_config =
    ['front_shock', 'Manitou'],
    ['rear_shock', 'Fox', false]]
    
+  
+# Listing 8.15, pg. 178
+
+module PartsFactory
+  def self.build(config:, part_class: Part, parts_class: Parts)
+    parts_class.new(
+      config.collect {|part_config|
+        part_class.new(
+          name:         part_config[0],
+          description:  part_config[1],
+          needs_spare:  part_config.fetch(2, true)
+        )
+      }
+    )
+  end
+end
